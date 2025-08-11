f0e06ff32a06400a523b0f13048f09925a7d1f51
unknown <133756075+lkgit1@users.noreply.github.com>
2025-08-11 20:16:11 +0100

    fix issue with using body status as status code

--- classes/Router.js
+++ classes/Router.js
@@ -99,11 +99,7 @@ export default class Router {
 
                 // Return HTTP response based on database query result
                 if (Array.isArray(queryResult) || typeof queryResult === 'object') {
-                    if (typeof queryResult === 'object' && queryResult.status) {
-                        return res.status(queryResult.status).json(queryResult)
-                    } else {
-                        return getResponse(200, res, queryResult)
-                    }
+                    return getResponse(200, res, queryResult)
                 } else {
                     // pass queryResult directly to getResponse 
                     // since queryResult is probably a statusCode at this point
